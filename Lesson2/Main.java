c633d98473346180a7591dbad0ceb048aa3d7cab
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-02-18 14:26:07 +0200

    Create Main.java

---
+++ Lesson2/Main.java
@@ -0,0 +1,41 @@
+package com.gmail.siniakboris;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Point pointOne = new Point(4, 2);
+		Point pointTwo = new Point(8, 4);
+		Point pointThree = new Point(10, 4);
+		Point pointFour = new Point(12, 2);
+
+		System.out.println("Точки:");
+		System.out.println("Расстояние 1 = " + pointOne.getDistance(pointTwo));
+		System.out.println("Расстояние 2 = " + pointOne.getDistance(pointThree));
+		System.out.println("Расстояние 3 = " + pointTwo.getDistance(pointFour));
+		System.out.println("Расстояние 4 = " + pointThree.getDistance(pointFour));
+		
+		System.out.println();
+
+		Square squareOne = new Square(pointOne, pointTwo);
+		System.out.println("Квадрат:");
+		System.out.println("Сторона квадрата = " + squareOne.getSide());
+		System.out.println("Периметр квадрата = " + squareOne.getPerimetr());
+		System.out.println("Площадь квадрата = " + squareOne.getArea());
+
+		System.out.println();
+		
+		Circle circleOne = new Circle(pointOne, pointTwo);
+		System.out.println("Круг:");
+		System.out.println("Периметр круга = " + circleOne.getPerimetr());
+		System.out.println("Площадь Круга = " + circleOne.getArea());
+		
+		System.out.println();
+		
+		Rectangle rectangleOne = new Rectangle(pointOne, pointTwo, pointThree, pointFour);
+		System.out.println("Прямоугольник:");
+		System.out.println("Периметр прямоугольника = " + rectangleOne.getPerimetr());
+		System.out.println("Площадь прямоугольника = " + rectangleOne.getArea());
+	}
+
+}
