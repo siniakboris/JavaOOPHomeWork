990a940c06f1664254fe92f6d4369cfe836d8aba
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-02-22 18:22:19 +0200

    Create Group.java

---
+++ Lesson4/Group.java
@@ -0,0 +1,107 @@
+package com.gmail.siniakboris;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Group {
+
+	private Student[] group = new Student[10];
+
+	public Group(Student[] group) {
+		super();
+		this.group = group;
+
+	}
+
+	public Group() {
+		super();
+	}
+
+	public Student[] getGroup() {
+		return group;
+	}
+
+	public void setGroup(Student[] group) {
+		this.group = group;
+	}
+	
+	public void addStudentManual() throws MyException {
+		
+		Student st = new Student ("имя", 0, "пол", 0);
+		Scanner sc = new Scanner(System.in);  
+		
+		System.out.println("Введите имя:");
+		st.setName(sc.nextLine());
+		System.out.println("Введите возраст:");
+		st.setAge(sc.nextInt());
+		System.out.println("Введите пол:");
+		st.setSex(sc.next());
+		System.out.println("Введите номер зачетки:");
+		st.setNumber(sc.nextInt());
+		
+		if (st.getAge() < 0 && st.getNumber() < 0) {
+			System.out.println("Ошибка, введены не коректные числовые значения");
+		}
+		
+		for (int i = 0; i < group.length; i++) {
+			if (group[i] == null) {
+				group[i] = st;
+				return;
+			}
+		}
+		throw new MyException();
+	}
+
+	public void sortStudent () {
+		Arrays.sort(group, (a, b) -> NullCheck.checkNull(a, b) == NullCheck.NOT_NULL ? a.getName().compareTo(b.getName()) 
+				: NullCheck.checkNull(a, b));
+	}
+	
+	
+	
+	public void addStudent(Student st) throws MyException {
+		if (st == null) {
+			throw new IllegalArgumentException("Студент не найден");
+		}
+		for (int i = 0; i < group.length; i++) {
+			if (group[i] == null) {
+				group[i] = st;
+				return;
+
+			}
+		}
+		throw new MyException();
+	}
+	
+	public void deleteStudent(int a) {
+		for (int i = 0; i < group.length; i++) {
+			if (group[i] != null && group[i].getNumber() == a) {
+				group[i] = null;
+				break;
+			}
+
+		}
+
+	}
+
+	public Student searchStudent(String name) {
+		for (int i = 0; i < group.length; i++) {
+			if (group[i] != null && group[i].getName().equals(name)) {
+				return group[i];
+			}
+		}
+		return null;
+
+	}
+
+	public void toPrintGroup() {
+		for (int i = 0; i < group.length; i++) {
+			if (group[i] != null) {
+				System.out.println("Номер зачетки: " + group[i].getNumber() + ", имя студента: " + group[i].getName()
+						+ ", возраст: " + group[i].getAge() + ", пол: " + group[i].getSex());
+			}
+		}
+
+	}
+
+}
