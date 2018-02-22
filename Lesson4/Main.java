6bfd9c5b0032189002c778e00b7ee036a7731aa2
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-02-22 18:22:42 +0200

    Create Main.java

---
+++ Lesson4/Main.java
@@ -0,0 +1,39 @@
+package com.gmail.siniakboris;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Student Ivan = new Student ("Иван", 19, "мужской", 1);
+		Student Lena = new Student ("Лена", 20, "женский", 2);
+		Student Anna = new Student ("Аня", 18, "женский", 2);
+		Student Bogdan = new Student ("Богдан", 17, "мужской", 2);
+		
+		Group groupOne = new Group();
+			
+		System.out.println();
+		System.out.println("Cостав группы:");
+		
+		try {
+		groupOne.addStudentManual();
+		groupOne.addStudent(Ivan);
+		groupOne.addStudent(Lena);
+		groupOne.addStudent(Anna);
+		groupOne.addStudent(Bogdan);
+
+		} catch (MyException e) {
+			System.out.println(e.toString());
+		}
+		
+		groupOne.sortStudent();
+		groupOne.toPrintGroup();
+		
+		System.out.println();
+		System.out.println("Найденные студенты");
+		System.out.println(groupOne.searchStudent("Иван"));
+		
+		groupOne.deleteStudent(1);
+		groupOne.deleteStudent(2);
+		
+	}
+
+}
