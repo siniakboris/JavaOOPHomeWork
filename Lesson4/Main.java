b7bf4dce80608f126d8779eec4ffda117854cdd2
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-02-23 20:49:52 +0200

    Update Main.java

--- Lesson4/Main.java
+++ Lesson4/Main.java
@@ -1,20 +1,22 @@
 package com.gmail.siniakboris;
 
+import java.util.Arrays;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		Student Ivan = new Student ("Иван", 19, "мужской", 1);
 		Student Lena = new Student ("Лена", 20, "женский", 2);
 		Student Anna = new Student ("Аня", 18, "женский", 2);
-		Student Bogdan = new Student ("Богдан", 17, "мужской", 2);
+		Student Bogdan = new Student ("Богдан", 19, "мужской", 2);
 		
 		Group groupOne = new Group();
 			
 		System.out.println();
 		System.out.println("Cостав группы:");
-		
+
 		try {
-		groupOne.addStudentManual();
+		//groupOne.addStudentManual();
 		groupOne.addStudent(Ivan);
 		groupOne.addStudent(Lena);
 		groupOne.addStudent(Anna);
@@ -30,6 +32,11 @@ public class Main {
 		System.out.println();
 		System.out.println("Найденные студенты");
 		System.out.println(groupOne.searchStudent("Иван"));
+		
+		System.out.println();
+		System.out.println("Поставить на учет в военкомат:");
+		
+		System.out.println(Arrays.toString(groupOne.voenkomSort()));
 		
 		groupOne.deleteStudent(1);
 		groupOne.deleteStudent(2);
