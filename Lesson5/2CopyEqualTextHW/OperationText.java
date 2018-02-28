ea3765eeba587fc6b838335042a6048c5c7609d0
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-02-28 13:25:26 +0200

    Create OperationText.java

---
+++ Lesson5/2CopyEqualTextHW/OperationText.java
@@ -0,0 +1,62 @@
+package com.gmail.siniakboris;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class OperationText {
+
+	public String[] wordToArray(File file) {
+		if (file != null) {
+
+			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+				String stringResult = "";
+				String text = "";
+
+				for (; (text = br.readLine()) != null;) {
+					stringResult = stringResult + text;
+				}
+
+				String[] result = stringResult.split("[ -,.!?\\s]+");
+				return result;
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public String getEqualWord(File fileOne, File fileTwo) {
+
+		String[] wordOne = wordToArray(fileOne);
+		String[] wordTwo = wordToArray(fileTwo);
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < wordOne.length; i++) {
+			for (int j = 0; j < wordTwo.length; j++) {
+				if (wordOne[i].equals(wordTwo[j])) {
+					sb.append(wordOne[i] + " ");
+					sb.toString();
+				}
+			}
+		}
+		return sb.toString();
+	}
+
+	public void saveEqualWordToFile(File fileOne, File fileTwo, File fileThree) {
+		String text = getEqualWord(fileOne, fileTwo);
+		if (text != null) {
+			try (PrintWriter pw = new PrintWriter(fileThree)) {
+				pw.println(text);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
