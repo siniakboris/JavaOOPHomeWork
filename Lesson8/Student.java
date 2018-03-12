ccdf5a47ec49012f7ce475f483b94ef4b314692d
Boris Siniak <35667330+siniakboris@users.noreply.github.com>
2018-03-12 12:42:20 +0200

    Create Student.java

---
+++ Lesson8/Student.java
@@ -0,0 +1,32 @@
+package com.gmail.siniakboris;
+
+import java.io.Serializable;
+
+public class Student extends Human implements Serializable {
+	
+	private int number;
+
+	public Student(String name, int age, String sex, int number) {
+		super(name, age, sex);
+		this.number = number;
+	}
+	
+
+	public Student(String name, int age, String sex) {
+		super(name, age, sex);
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	@Override
+	public String toString() {
+		return "Номер зачётки = " + number + super.toString();
+	}
+	
+}
